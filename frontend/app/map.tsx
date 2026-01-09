ca1986a42b5fc754fdbf54f52de379a173807012
Andrew Li <andrew.li2277@gmail.com>
2026-01-09 20:59:25 +1100

    fix maps theme on android

--- frontend/app/map.tsx
+++ frontend/app/map.tsx
@@ -107,6 +107,7 @@ export default function MapScreen() {
           </View>
         ) : (
           <MapView
+            key={theme}
             ref={mapRef}
             style={styles.map}
             initialRegion={{
